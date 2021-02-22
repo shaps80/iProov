c915044632a597b46ce521cee4781602c3a1df56
Shaps Benkau <shapsuk@me.com>
2021-02-22 15:11:56 +0000

    Adds checksum

--- Package.swift
+++ Package.swift
@@ -12,7 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "iProov",
-            url: "https://github.com/shaps80/iProov/blob/master/iProov.xcframework.zip"
+            url: "https://github.com/shaps80/iProov/blob/master/iProov.xcframework.zip",
+            checksum: "a2e38401f8bdcdd8c9c222e0991fe6640356710b38beb186b536e43a016c5624"
         ),
     ]
 )
