43def39c96442e884f5b34a6122a84f1921f0456
Shaps Benkau <shapsuk@me.com>
2021-02-22 15:24:34 +0000

    XCFramework is now distributed through releases. Updates checksum

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "iProov",
-            url: "https://github.com/shaps80/iProov/blob/master/iProov.xcframework.zip",
+            url: "https://github.com/shaps80/iProov/releases/download/8.2.1/iProov.xcframework.zip",
             checksum: "a2e38401f8bdcdd8c9c222e0991fe6640356710b38beb186b536e43a016c5624"
         ),
     ]
